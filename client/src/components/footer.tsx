71f702d0d55e62c96be4f3d61dbeb03e556d94f3
backdfb <wva22lfr@gmail.com>
2024-05-07 20:08:34 +0900

    footer 1차 작성

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-
 function Footer() {
   return (
     <footer className="bg-gray-500 text-white text-center p-4">
       <div>
-        <p>플랜픽</p>
-        <p>성장동행자</p>
-        <a className="text-white">고객센터</a> | 
-        <a href="mailto:help@planpeak.co.kr" className="text-white">help@planpeak.co.kr</a> <br/>
-        <a className="text-white">개인정보 처리방침</a>
+        <p className="text-center">플랜픽</p> <br/>
+        <p className="text-center">성장동행자</p> <br/>
+        <a className="text-white text-center">고객센터</a> <br/>
+        <a href="mailto:help@planpeak.co.kr" className="text-white text-center">help@planpeak.co.kr</a> <br/>
+        <a className="text-white text-center">개인정보 처리방침</a>
       </div>
     </footer>
   );
