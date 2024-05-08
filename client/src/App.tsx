6a9087543a9114eb89efc7f2c9c8c904744ffbeb
backdfb <wva22lfr@gmail.com>
2024-05-08 17:05:01 +0900

    app.tsx 원복

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Router>
-        <Header />
         <Routes>
           <Route path="/" Component={Home}></Route>
         </Routes>
