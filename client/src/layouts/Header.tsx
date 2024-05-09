849ff722c079a374e870a0d3a29a7e0d54c1b964
inyeong <marineyeong@gmail.com>
2024-05-09 15:49:36 +0900

    Rename: "헤더, 푸터, 사이드네비 component -> layouts 폴더 수정"

--- client/src/layouts/Header.tsx
+++ client/src/layouts/Header.tsx
@@ -3,8 +3,8 @@ import Logo from '../assets/logo/planpeakHorizontal.jpg';
 
 function Header() {
   return (
-    <div className='flex items-center justify-between mx-20 py-1'>
-      <ul className='flex p-1 items-center space-x-4 text-ppVeryLightGray'>
+    <div className="flex items-center justify-between mx-20 py-1">
+      <ul className="flex p-1 items-center space-x-4 text-ppVeryLightGray">
         <li>
           <Link to="/">
             <img style={{ width: '126px' }} src={Logo} alt="logo" />
@@ -12,10 +12,16 @@ function Header() {
         </li>
         <li>
           <Link to="/">모임 참가하기</Link>
-          <ul className='hidden'>
-            <li><a href='#'>모든 모임 둘러보기</a></li>
-            <li><a href='#'>내가 참여중인 모임</a></li>
-            <li><a href='#'>나의 모임 이력</a></li>
+          <ul className="hidden">
+            <li>
+              <a href="#">모든 모임 둘러보기</a>
+            </li>
+            <li>
+              <a href="#">내가 참여중인 모임</a>
+            </li>
+            <li>
+              <a href="#">나의 모임 이력</a>
+            </li>
           </ul>
         </li>
         <li>
@@ -25,7 +31,7 @@ function Header() {
           <Link to="/">플랜픽 소개</Link>
         </li>
       </ul>
-      <div className='flex p-2 space-x-4 font-semibold text-ppVeryLightGray'>
+      <div className="flex p-2 space-x-4 font-semibold text-ppVeryLightGray">
         <button className="bg-transparent hover:bg-ppBlue font-semibold hover:text-ppWhite py-1 px-4 border border-ppBlack hover:border-transparent rounded-full">
           <Link to="/">로그인</Link>
         </button>
@@ -34,8 +40,6 @@ function Header() {
         </button>
       </div>
     </div>
-
-
   );
 }
 
--- client/src/layouts/SideNav.tsx
+++ client/src/layouts/SideNav.tsx
@@ -1,9 +1,9 @@
-import { Link } from 'react-router-dom';
+import { Link, Router } from 'react-router-dom';
 
 function SideNav() {
   return (
     <div className="py-7 px-5">
-    <ul className="text-ppBlack text-2xl font-bold py-5">마이페이지</ul>
+      <ul className="text-ppBlack text-2xl font-bold py-5">마이페이지</ul>
       <Link to={'/'}>
         <li className="list-none text-ppGray text-lg font-normal leading-10 hover:text-ppBlue">
           관리자 프로필 설정
