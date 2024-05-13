88de8afc95d6f749bc48d7e173b83e4265a3d92f
backdfb <wva22lfr@gmail.com>
2024-05-14 07:51:43 +0900

    landingpage 작성

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,5 +1,51 @@
 import Image1 from '../assets/logo/landing1.png'
 import Image2 from '../assets/logo/landing2.png'
+import CardImage from '../components/Common/Card'
+
+interface Event {
+    imageSrc: string;
+    name: string;
+    date: string;
+    description: string;
+}
+
+const events: Event[] = [
+    {
+        name: '커피 원데이 클래스',
+        description: '커피를 좋아하는 사람들의 모임',
+        date: '을지로, 2024. 4. 29',
+        imageSrc: 'path_to_image',
+    },
+    {
+        name: '드로잉 모임',
+        description: '그림을 그려봐요',
+        date: '을지로, 2024. 4. 29',
+        imageSrc: 'path_to_image',
+    },
+    {
+        name: '영화 평론 모임',
+        description: '영화관람 좋아 하시죠?',
+        date: '을지로, 2024. 4. 29',
+        imageSrc: 'path_to_image',
+    },
+    {
+        name: '문문문',
+        description: '달과 별을 보러 갈까요?',
+        date: '을지로, 2024. 4. 29',
+        imageSrc: 'path_to_image',
+    },
+];
+
+const renderItems = (items: Event[]) => {
+    return items.map((item) => (
+      <li key={item.name} className="col text-left items-center">
+        <CardImage src={item.imageSrc} alt={item.altText || item.name} />
+        <span className='text-sm text-bold'>{item.name}</span> <br />
+        <span className='text-sm'>{item.description}</span> <br />
+        <span className='text-sm'>{item.date}</span>
+      </li> 
+    ));
+};
 
 function LandingPage() {
     return (
@@ -22,14 +68,22 @@ function LandingPage() {
                     이메일 자동화로 번거로운 알림 설정을 최소화 해보세요
                 </span>
             </div>
-            <div>
-
+            <div className='flex flex-auto gab-4'>
+                <div className='mt-10 ml-80 text-center'>
+                    <h1 className='text-3xl text-bold mt-10'>출석과 챌린지를 <br/> 한눈에 확인하세요</h1>
+                    <p className='mt-2 text-bold text-ppVeryLightGray'>직관적인 디자인으로 보여드립니다.</p>
+                </div>
+                <div className='mt-10 mb-5 ml-5'>
+                  <CardImage src='path_to_image' title="" />
+                </div>
             </div>
-            <div>
-
+            <div className='mt-10 mb-10'>
+                <h1 className='text-3xl text-center text-bold'>다른 모임장들은 <br /> 이렇게 이용중이에요</h1>
             </div>
-            <div>
-                
+            <div className='flex flex-auto gab-4 mb-10'>
+                <ul className='mt-10 ml-2 grid grid-cols-4 gap-4'>
+                    {renderItems(events)}
+                </ul>
             </div>
         </div>
 
