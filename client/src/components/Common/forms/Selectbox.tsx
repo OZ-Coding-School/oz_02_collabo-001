9d0f4dc1a626a9845f746c285d056bc684272103
inyeong <marineyeong@gmail.com>
2024-05-14 10:35:05 +0900

    Fix: option key 수정

--- client/src/components/Common/forms/Selectbox.tsx
+++ client/src/components/Common/forms/Selectbox.tsx
@@ -2,7 +2,7 @@ function Selectbox(props: any) {
   return (
     <select className="p-2 border border-solid border-ppVeryLightGray rounded overflow-hidden">
       {props.options.map((option: any) => (
-        <option key={option} value={option.value}>
+        <option key={option.value} value={option.value}>
           {option.name}
         </option>
       ))}
