634b028d26104f39e4c3fc02d70902823d07e669
inyeong <marineyeong@gmail.com>
2024-05-14 10:27:06 +0900

    Fix: "옵션 key 값 추가"

--- client/src/components/Common/forms/Selectbox.tsx
+++ client/src/components/Common/forms/Selectbox.tsx
@@ -2,7 +2,9 @@ function Selectbox(props: any) {
   return (
     <select className="p-2 border border-solid border-ppVeryLightGray rounded overflow-hidden">
       {props.options.map((option: any) => (
-        <option value={option.value}>{option.name}</option>
+        <option key={option} value={option.value}>
+          {option.name}
+        </option>
       ))}
     </select>
   );
