f91fbc1d1c8d171441230ccea721b15bf56018ea
inyeong <marineyeong@gmail.com>
2024-05-14 17:06:29 +0900

    Feat: "헤더 Link 연결 및 main Route 수정"

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,16 +7,23 @@ import LogInSelectPage from './pages/login/LogInSelectPage.tsx';
 import SignUp from './pages/login/SignUp.tsx';
 import LogIn from './pages/login/LogIn.tsx';
 import LandingPage from './pages/LandingPage.tsx';
+import MeetingList from './pages/MeetingList.tsx';
+import MainPage from './pages/MainPage.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/landing' element={<LandingPage />} />
-        <Route path='/loginselect' element={<LogInSelectPage />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/login' element={<LogIn />} />
+        <Route element={<App />}>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/meetingList" element={<MeetingList />} />
+        </Route>
+      </Routes>
+      <Routes>
+        <Route path="/landing" element={<LandingPage />} />
+        <Route path="/loginselect" element={<LogInSelectPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
