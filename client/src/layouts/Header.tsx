c5778906982bf93f0a3a64cea3d2af9027f7f44d
backdfb <wva22lfr@gmail.com>
2024-05-17 19:48:05 +0900

    header 햄버거 작업중/tailwind.config.js 스크린 크기 설정

--- client/src/layouts/Header.tsx
+++ client/src/layouts/Header.tsx
@@ -1,38 +1,54 @@
-import { Link, Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Logo from '../assets/logo/planpeakHorizontal.jpg';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Header() {
- 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userName, setUserName] = useState('');
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    // 메뉴 토글 함수
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+
+    // 메뉴 열기/닫기 버튼에 이벤트 리스너 추가
+    const burger = document.querySelector('.navbar-burger');
+    burger?.addEventListener('click', toggleMenu);
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      burger?.removeEventListener('click', toggleMenu);
+    };
+  }, [isMenuOpen]);
 
   return (
     <header>
       <div className="content flex items-center justify-between py-1">
-        <ul className="flex p-1 items-center space-x-4">
+        <ul className={`flex p-1 items-center space-x-4 ${isMenuOpen ? "hidden" : "block"} md:block`}>
           <li>
             <Link to="/">
-              <img style={{ width: '126px' }} src={Logo} alt="logo" />
+              <img style={{ width: '126px' }} src={Logo} alt="PlanPeak logo" />
             </Link>
           </li>
-          <li>
+          <li className='md:flex p-1 items-center space-x-4'>
             <Link to="/user/meetinglist" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
               모임 참가하기
             </Link>
-            <ul className="hidden">
+            <ul className="hidden md:flex items-center space-x-1">
               <li>
-                <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
+                <Link to="/browse" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
                   모든 모임 둘러보기
                 </Link>
               </li>
               <li>
-                <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
+                <Link to="/participations" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
                   내가 참여중인 모임
                 </Link>
               </li>
               <li>
-                <Link to="/" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
+                <Link to="/history" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
                   나의 모임 이력
                 </Link>
               </li>
@@ -44,12 +60,20 @@ function Header() {
             </Link>
           </li>
           <li>
-            <Link to="/landing" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
+            <Link to="/about" className="p-2 text-ppVeryLightGray hover:text-ppBlue">
               플랜픽 소개
             </Link>
           </li>
         </ul>
-        <div className="flex p-2 space-x-4">
+        <div className='lg:hidden'>
+            <button className='navbar-burger flex items-center text-blue-600 p-3'>
+              <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <title>Mobile menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+              </svg>
+            </button>
+        </div>
+        <div className='flex p-2 space-x-4 md:flex'>
           {isLoggedIn ? (
             <>
               <span className="overflow-hidden rounded-full">
@@ -67,7 +91,7 @@ function Header() {
                   로그아웃
                 </Link>
               </span>
-            </>    
+            </>
           ) : (
             <>
               <span className="overflow-hidden border border-solid border-ppVeryLightGray rounded-full hover:border-transparent hover:bg-ppBlue">
@@ -81,7 +105,7 @@ function Header() {
                 </Link>
               </span>
             </>
-          )}  
+          )}
         </div>
       </div>
     </header>
